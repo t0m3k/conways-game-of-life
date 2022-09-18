d99d7c466d82a9058904f6878013916f3b152be8
Tomasz Tracz <t@tracz.me>
2022-09-18 22:23:57 +0100

    Display output

--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
+const arr = new GameOfLife(150, 150);
+const width = 600;
+const height = 600;
 function setup() {
-  let cnv = createCanvas(800, 600);
+  let cnv = createCanvas(width, height);
   cnv.parent("myContainer");
+  frameRate(15);
 }
 
+console.log(arr.x, arr.y);
+
 function draw() {
-  background(0);
+  background(15);
+  for (let x = 0; x < arr.x; x++) {
+    for (let y = 0; y < arr.y; y++) {
+      arr.item(x, y)?.next();
+      const element = arr.item(x, y);
+      const elementState = element.state();
+
+      switch (elementState) {
+        case state.ALIVE:
+          fill(255);
+          break;
+        // case state.COMING_TO_LIFE:
+        //   fill(0, 255, 0);
+        //   break;
+        case state.DYING:
+          fill(255);
+          break;
+
+        default:
+          fill(0);
+          break;
+      }
+
+      rect(
+        1 + ((width - 2) / arr.x) * x,
+        1 + ((height - 2) / arr.y) * y,
+        width / arr.x,
+        height / arr.y
+      );
+    }
+  }
+  for (let x = 0; x < arr.x; x++) {
+    for (let y = 0; y < arr.y; y++) {}
+  }
 }
