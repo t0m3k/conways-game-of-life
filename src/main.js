b43ea5d834749fb0fa004bc6c9212e0abfedc8aa
Tomasz Tracz <t@tracz.me>
2022-09-21 14:13:35 +0100

    Draw using forEach

--- src/main.js
+++ src/main.js
@@ -13,47 +13,41 @@ function setup() {
 function draw() {
   gameState.checkState();
 
+  let alive = 0;
+  game.forEach((element, x, y) => {
+    const elementState = element.state();
+
+    switch (elementState) {
+      case cellState.ALIVE:
+        fill(255);
+
+        alive++;
+        break;
+      case cellState.COMING_TO_LIFE:
+        fill(0);
+        break;
+      case cellState.DYING:
+        fill(255);
+
+        alive++;
+        break;
+
+      default:
+        fill(0);
+        break;
+    }
+
+    rect(
+      1 + ((width - 2) / game.x) * x,
+      1 + ((height - 2) / game.y) * y,
+      width / game.x,
+      height / game.y
+    );
+  });
+
   if (gameState.state === GameState.states.RUNNING) {
-    let alive = 0;
-    for (let x = 0; x < game.x; x++) {
-      for (let y = 0; y < game.y; y++) {
-        const element = game.item(x, y);
-        const elementState = element.state();
-
-        switch (elementState) {
-          case cellState.ALIVE:
-            fill(255);
-
-            alive++;
-            break;
-          case cellState.COMING_TO_LIFE:
-            fill(0);
-            break;
-          case cellState.DYING:
-            fill(255);
-
-            alive++;
-            break;
-
-          default:
-            fill(0);
-            break;
-        }
-
-        rect(
-          1 + ((width - 2) / game.x) * x,
-          1 + ((height - 2) / game.y) * y,
-          width / game.x,
-          height / game.y
-        );
-      }
-    }
-
-    for (let x = 0; x < game.x; x++) {
-      for (let y = 0; y < game.y; y++) {
-        game.item(x, y).next();
-      }
-    }
-    $("#aliveCells").html(alive);
+    game.forEach((item) => item.next());
   }
+
+  $("#aliveCells").html(alive);
 }
